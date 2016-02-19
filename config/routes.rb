62f5d3e03e24c63c02859c76e42c4674830e8da1
Raghu Varma R <rrv461@gmail.com>
2016-02-19 09:05:33 +0530

    added welocme controller for landing or welcome for this application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #get 'welcome/index'
+
   resources :posts
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
