965c7c1c02b021bac2bf9d9d0041076b27f7808e
Raghu Varma R <rrv461@gmail.com>
2016-02-25 03:21:44 +0530

    CA: Added set headers method in application controller to setup before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,26 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception
-  protect_from_forgery
-  #respond_to(:html, :json)
-  respond_to :html, :xml, :json
-
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	#protect_from_forgery with: :exception
+	protect_from_forgery
+	#respond_to(:html, :json)
+	respond_to :html, :xml, :json
+	before_filter :set_headers
 	after_filter  :set_csrf_cookie_for_ng
+
+	def set_headers
+	  puts 'PostsController.set_headers'
+	  if request.headers["HTTP_ORIGIN"]     
+	  # better way check origin
+	  # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+	    headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+	    headers['Access-Control-Expose-Headers'] = 'ETag'
+	    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+	    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+	    headers['Access-Control-Max-Age'] = '86400'
+	    headers['Access-Control-Allow-Credentials'] = 'true'
+	  end
+	end
 
 	def set_csrf_cookie_for_ng
 	cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
